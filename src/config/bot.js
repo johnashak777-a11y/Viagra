34c83e6ba0ac5e2f0ae55c037d7c25bceb141da8
johnashak777-a11y <johnashak777@gmail.com>
2026-06-22 01:12:36 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "with my dihh",
+        name: "WITH SONIC",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 5,
       },
     ],
   },
